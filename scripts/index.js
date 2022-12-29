8e818d020b4631aef37459a97674b3fa335a7525
Elizaveta Efremova <efremova.elz.pvl@gmail.com>
2022-12-30 02:41:43 +0500

    added nojekyll

---
+++ scripts/index.js
@@ -0,0 +1,48 @@
+//Реализация кнопки-добавления
+const aboutButton = document.querySelector('.profile__edid-button');
+//Реализация попапа
+const aboutPopup = document.querySelector('.popup');
+//Реализация кнопки-закрытия
+const aboutCloseButton = document.querySelector('.popup__close-button');
+
+const buttonSave = document.querySelector('.popup__save-button');
+const profileName = document.querySelector('.profile__name');
+const profileАctivity = document.querySelector('.profile__activity');
+const inputName = document.querySelector('[name="input-name"]');
+const inputАctivity = document.querySelector('[name="input-аctivity"]');
+const profileInfo = document.querySelector('.popup__profile-text');
+
+aboutButton.addEventListener('click', (Event) => {
+    aboutPopup.classList.add('popup_opened');
+    inputName.value = profileName.textContent;
+    inputАctivity.value = profileАctivity.textContent;
+})
+
+
+function closePopup() {
+    aboutPopup.classList.remove('popup_opened');
+};
+
+aboutCloseButton.addEventListener('click', closePopup);
+
+//Сохранение информации в профиле
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+
+    profileName.textContent = inputName.value;
+    profileАctivity.textContent = inputАctivity.value;
+
+    closePopup();
+};
+
+profileInfo.addEventListener('submit', handleFormSubmit);
+
+//Изменение цвета сердца
+
+const aboutHeart = Array.from(document.querySelectorAll('.photo__heart'));
+
+aboutHeart.forEach((button) => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('photo__heart_active');
+    })
+})
